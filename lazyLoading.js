99633908ca5410fa2f5f276f6e3abdf2f69d28c7
Mwangi-dan <d.ndungu@alustudent.com>
2023-07-31 09:45:10 +0300

    Lazuy Loading

---
+++ lazyLoading.js
@@ -0,0 +1,24 @@
+function lazyLoadImages() {
+    const images = document.querySelectorAll('.screen img[loading="lazy"]');
+    
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+    
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.removeAttribute('loading'); // Remove the "loading" attribute
+          imageObserver.unobserve(img);
+        }
+      });
+    }, options);
+    
+    images.forEach(img => imageObserver.observe(img));
+  }
+  
+  // Call the function to start lazy loading the images
+  lazyLoadImages();
